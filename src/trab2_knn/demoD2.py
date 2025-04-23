a4242e982f19e2c623b31cbb0ef7d831dc6c3ff3
Eduardo Reinert <eduardo.a.reinertt@gmail.com>
2025-04-23 19:40:03 -0300

    DemoD2 lindo

--- src/trab2_knn/demoD2.py
+++ src/trab2_knn/demoD2.py
@@ -32,65 +32,15 @@ def grupo_de_dados_2():
     resultado = testa_valores_de_k(grupoTrain, trainRots, grupoTest, testRots)
     melhor_k, melhor_acuracia = resultado["melhor_k"], resultado["melhor_acuracia"]
 
-    # print("--------")
-    # for i in grupoTrain:
-    #     for num in i:
-    #         print(f"{num:.2f}, ", end="")
-    #     print("")
-    # print("--------")
-
-    # for i in grupoTest:
-    #     for num in i:
-    #         print(f"{num:.2f}, ", end="")
-    #     print("")
-
     # Q2.1: Aplique seu kNN a este problema. Qual é a sua acurácia de classificação?
     print(f"k de maior acurácia: {melhor_k} ({melhor_acuracia})")
-
-    # min_max_list = [
-    #     {'min': np.min(grupoTrain[:, col]), 'max': np.max(grupoTrain[:, col])} for col in range(grupoTrain.shape[1])
-    # ]
 
     grupoTrain = (grupoTrain - grupoTrain.min(axis=0)) / (grupoTrain.max(axis=0) - grupoTrain.min(axis=0))
     grupoTest = (grupoTest - grupoTest.min(axis=0)) / (grupoTest.max(axis=0) - grupoTest.min(axis=0))
 
-    # Initialize an empty list to store the normalized test data
-    # test_data_normalized = []
-
-    # Loop through each row of the test data
-    # for row in grupoTest:
-    #     normalized_row = []
-
-    #     # Loop through each column value in the row
-    #     for col, value in enumerate(row):
-    #         # Get the min and max for the current column from the training data
-    #         column_min = min_max_list[col]['min']
-    #         column_max = min_max_list[col]['max']
-
-    #         # Apply the Min-Max scaling formula
-    #         normalized_value = (value - column_min) / (column_max - column_min)
-
-    #         # Append the normalized value to the normalized row
-    #         normalized_row.append(normalized_value)
-
-    #     # Append the normalized row to the list of normalized test data
-    #     test_data_normalized.append(normalized_row)
-
     # Testar valores de k para obter a melhor correspondência
     resultado = testa_valores_de_k(grupoTrain, trainRots, grupoTest, testRots)
     melhor_k, melhor_acuracia = resultado["melhor_k"], resultado["melhor_acuracia"]
-    # print("--------")
-    #
-    # for i in grupoTrain:
-    #     for num in i:
-    #         print(f"{num:.2f}, ", end="")
-    #     print("")
-    # print("--------")
-    #
-    # for i in grupoTest:
-    #     for num in i:
-    #         print(f"{num:.2f}, ", end="")
-    #     print("")
 
     # Q2.1: Aplique seu kNN a este problema. Qual é a sua acurácia de classificação?
     print(f"k de maior acurácia: {melhor_k} ({melhor_acuracia})")
@@ -101,7 +51,6 @@ def grupo_de_dados_2():
 
 
 def main():
-
     grupo_de_dados_2()
 
 if __name__ == "__main__":
