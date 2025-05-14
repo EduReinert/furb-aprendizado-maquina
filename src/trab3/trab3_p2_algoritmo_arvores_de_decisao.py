3b537cfac600736cd52ac156f0ee93078880c5fa
Eduardo Reinert <eduardo.a.reinertt@gmail.com>
2025-05-14 17:08:12 -0300

    Trabalho 3: Parte 2 (em progresso)

---
+++ src/trab3/trab3_p2_algoritmo_arvores_de_decisao.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+import plotly.express as px
+import pickle
+
+
+def main():
+    print('hello')
+
+if __name__ == "__main__":
+    main()
