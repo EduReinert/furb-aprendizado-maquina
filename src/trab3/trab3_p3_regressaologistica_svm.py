a436fdaed24d71d5159a25030223a6f6d7a8073a
Eduardo Reinert <eduardo.a.reinertt@gmail.com>
2025-05-18 11:12:18 -0300

    Regressão Logística (risco_credito.pkl)

--- src/trab3/trab3_p3_regressaologistica_svm.py
+++ src/trab3/trab3_p3_regressaologistica_svm.py
@@ -14,7 +14,7 @@ Eduardo Reinert
 Vinicius Vanelli
 """
 
-def carregar_dados_pkl():
+def regressao_logistica():
     #1. Utilize a base de dados construída no Trabalho 3 ‘risco_credito.pkl’, que possui 14 registros, para testar o algoritmo de Regressão Logística.
     arquivo_encoded = 'risco_credito.pkl'
 
@@ -28,15 +28,36 @@ def carregar_dados_pkl():
     
     X_filtrado = X[mascara]
     y_filtrado = y[mascara]
+    print(f'Total de registros: ', len(X_filtrado))
     
-    return X_filtrado, y_filtrado
+    #3. Treine o algoritmo de regressão logística e utilize o parâmetro ‘random_state =1’ para ter sempre o mesmo resultado.
+    regressao_logistica = LogisticRegression(random_state=1)
+    regressao_logistica = regressao_logistica.fit(X_filtrado, y_filtrado)
+    
+    #4. Utilize o comando ‘.intercept_’ para ter o resultado do B0.
+    #O resultado deve ser =-0.80828993
+    print(f'Resultado comando .intercept_: ', regressao_logistica.intercept_)
+    
+    #5. Utilize o comando ‘.coef_’ para ter o resultado dos demais parâmetros que deve ser:
+    #array([[-0.76704533,  0.23906678, -0.47976059,  1.12186218]])
+    print(f'Resultado comando .coef_: ', regressao_logistica.coef_)
+    
+    #6. Agora utilize o comando ‘predict’ para fazer o teste do seu algoritmo com:
+
+
+    # a) história boa, dívida alta, garantias nenhuma, renda > 35 == BAIXO
+    previsao_01 = regressao_logistica.predict([[0, 0, 1, 2]])
+    print(f'Esperado: baixo \nObtido:', previsao_01)
+    
+    # b) história ruim, dívida alta, garantias adequada, renda < 15 == ALTO
+    previsao_02 = regressao_logistica.predict([[2, 0, 0, 0]])
+    print(f'Esperado: alto \nObtido:', previsao_02)
 
 def main():
-    """
-    Regressão Logística
-    """
-    X_risco_credito, y_risco_credito = carregar_dados_pkl()
-    print (len(X_risco_credito))
-
+    # PARTE 3: Regressão Logística (risco_credito.pkl)
+    regressao_logistica('risco_credito.pkl')
+    
+    
+    
 if __name__ == "__main__":
     main()
